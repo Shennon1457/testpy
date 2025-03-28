64b94bfe463c86bdf7d14fdb2478a04eea856396
Shennon1457 <oxana.dash@gmail.com>
2025-03-28 15:19:59 +0300

    second

--- test/test_calc.py
+++ test/test_calc.py
@@ -1,31 +1,58 @@
 from src.calc import Calculator as bas
+import pytest 
 
 calc = bas()
 
-def test_sum():
-    assert calc.sum(1, 2) == 3
-    assert calc.sum(4, 5) == 9
-    assert calc.sum(3, -3) == 0
-    assert calc.sum(3, 3) == 6
-    assert calc.sum(-3.5, 3) == -0.5
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        (3, 5, 8),
+        (6, 5, 11),
+        (3, 7, 10),
+        (8, 10, 18),
+        (15, 4, 19)
+    ]
+)
 
-def test_sub():
-    assert calc.subtract(8, 2) == 6
-    assert calc.subtract(5, 2) == 3
-    assert calc.subtract(9, 4) == 5
-    assert calc.subtract(8, -4) == 12
-    assert calc.subtract(15.5, 10) == 5.5
-
-def test_mult():
-    assert calc.multiply(6, 5) == 30
-    assert calc.multiply(5, -5) == -25
-    assert calc.multiply(10, 2) == 20
-    assert calc.multiply(6, 2) == 12
-    assert calc.multiply(5, 2) == 10
-
-def test_divide():
-    assert calc.divide(20, 10) == 2
-    assert calc.divide(81, 9) == 9
-    assert calc.divide(36, 6) == 6
-    assert calc.divide(55, 5) == 11
-    assert calc.divide(60, 6) == 10
+def test_sum(a, b, expected):
+    assert calc.sum(a, b) == expected
+    
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        (3, 5, -2),
+        (6, 5, 1),
+        (3, 7, -4),
+        (18, 10, 8),
+        (15, 4, 11)
+    ]
+)
+def test_sub(a, b, expected):
+    assert calc.subtract(a, b) == expected
+    
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        (3, 5, 15),
+        (6, 5, 30),
+        (3, 7, 21),
+        (18, 10, 180),
+        (15, 4, 60)
+    ]
+)
+def test_mult(a, b, expected):  
+    assert calc.multiply(a, b) == expected
+    
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        (4, 2, 2),
+        (6, 1, 6),
+        (10, 2, 5),
+        (18, 10, 1.8),
+        (12, 4, 3)
+    ]
+)
+def test_divide(a, b, expected):
+    assert calc.divide(a, b) == expected
+    
